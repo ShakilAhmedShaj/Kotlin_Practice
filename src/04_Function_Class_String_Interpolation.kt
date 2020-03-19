d7338922e530fd978da5d72bd3ad5a691cda275a
SHAZ <shakilahmedshaj@gmail.com>
2020-03-20 01:50:14 +0600

    till 04

---
+++ src/04_Function_Class_String_Interpolation.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var personObj = Person()
+    personObj.name = "Steve"
+
+    print("The name of the person is ${personObj.name}")
+}
+
+class Person {
+
+    var name: String = ""
+}
